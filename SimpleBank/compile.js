e31bb041d075080d3bfbdfb882d91eedc1cbfe71
Farshad <f_ghayati@hotmail.com>
2019-04-16 01:33:47 +0200

    Move folder to SC

--- SimpleBank/compile.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const fs = require('fs');
-const solc = require('solc');
-
-const SimpleBankPath = path.resolve(__dirname, 'contracts', 'SimpleBank.sol');
-const source = fs.readFileSync(SimpleBankPath, 'utf8');
-
-module.exports = solc.compile(source, 1).contracts[':SimpleBank'];
